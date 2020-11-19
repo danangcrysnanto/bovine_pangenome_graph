24c0d81c93b5d88ccdb063782b9854dbc6ac37d5
danangcrysnanto <danang.crysnanto@usys.ethz.ch>
2020-11-19 16:07:48 +0100

    Fix to include all SVs for annot and viz

--- subworkflows/sv_analysis.py
+++ subworkflows/sv_analysis.py
@@ -128,8 +128,8 @@ localrules: create_breakpoint_bed
 rule create_breakpoint_bed:
     input:
         bubble_file = "analysis/bubble/{asb}_bubble.tsv",
-        bialsv_file = "analysis/bubble/{asb}_bialsv_stat.tsv",
-        multisv_file = "analysis/bubble/{asb}_multisv_stat.tsv"
+        bialsv_file = "analysis/bubble/{asb}_biallelic_sv.tsv",
+        multisv_file = "analysis/bubble/{asb}_multiallelic_sv.tsv"
     output:
         left_bed = "analysis/bubble/{asb}_left_breakpoints.bed",
         right_bed = "analysis/bubble/{asb}_right_breakpoints.bed"
@@ -144,15 +144,28 @@ rule create_breakpoint_bed:
                 # only get the numeric part of the chromo
                 chromo = [int(x) for x in chromo.split("_") if re.search(r"\d+", x)][0]
                 right_bp[f"{chromo}_{left_side}"] = right_side
-        # process the biallelic breakpoints
 
-        def wrote_sv_bed(line, left_file, right_file):
-            sv_comp, *sv_rest = line.strip().split()
-            _, *chromo, leftcoord = sv_comp.split("_")
-            # only get the numeric part as the chromosome id
-            chromo = [int(x) for x in chromo if re.search(r"\d+", x)][0]
-            leftcoord = int(leftcoord)
-            start_node, *_, stop_node = sv_rest[-2].split(",")
+        def wrote_sv_bed(line, left_file, right_file, mutype="biallelic"):
+            if mutype == "biallelic":
+                # 1 165873 AltDel 497 2 s1 s2 s133016 s3
+                line_comp = line.strip().split()
+                chromo, leftcoord = line_comp[:2]
+                start_node, stop_node = line_comp[5], line_comp[-1]
+                sv_comp = f"{chromo}_{leftcoord}"
+                leftcoord = int(leftcoord)
+                # sv_comp, *sv_rest = line.strip().split()
+                # _, *chromo, leftcoord = sv_comp.split("_")
+                # only get the numeric part as the chromosome id
+                # chromo = [int(x) for x in chromo if re.search(r"\d+", x)][0]
+                # start_node, *_, stop_node = sv_rest[-2].split(",")
+            if mutype == "multiallelic":
+                # 1_535561        1898    5138    AltIns  s33,s60173,s36
+                line_comp = line.strip().split()
+                chromo, leftcoord = line_comp[0].split("_")
+                leftcoord = int(leftcoord)
+                sv_comp = f"{chromo}_{leftcoord}"
+                start_node, *_, stop_node = line_comp[-1].split(",")
+            # write the bed file
             left_file.write(
                 (f"{chromo}\t{leftcoord-1}\t{leftcoord+1}\t{start_node}\t{stop_node}\t{sv_comp}\n"))
             svid = f"{chromo}_{leftcoord}"
@@ -163,19 +176,21 @@ rule create_breakpoint_bed:
         with open(input["bialsv_file"]) as bialfile, open(input["multisv_file"]) as multifile:
             with open(output["left_bed"], "a") as left_file, open(output["right_bed"], "a") as right_file:
                 for line in bialfile:
-                    wrote_sv_bed(line, left_file, right_file)
+                    # process the biallelic breakpoints
+                    wrote_sv_bed(line, left_file, right_file, mutype="biallelic")
             # process the multiallelic breakpoints
                 sv_processed = []
                 mutlist = []
                 for line in multifile:
-                    sv_comp, *sv_rest = line.strip().split()
-                    _, *chromo, leftcoord = sv_comp.split("_")
-                    # only get the numeric part as the chromosome id
-                    chromo = [int(x) for x in chromo if re.search(r"\d+", x)][0]
-                    svid = f"{chromo}_{leftcoord}"
+                    svid = line.strip().split()[0]
+                    # sv_comp, *sv_rest = line.strip().split()
+                    # _, *chromo, leftcoord = sv_comp.split("_")
+                    # # only get the numeric part as the chromosome id
+                    # chromo = [int(x) for x in chromo if re.search(r"\d+", x)][0]
+                    # svid = f"{chromo}_{leftcoord}"
                     if svid not in sv_processed:
                         sv_processed.append(svid)
-                        wrote_sv_bed(line, left_file, right_file)
+                        wrote_sv_bed(line, left_file, right_file, mutype="multiallelic")
 
 localrules: annotate_breakpoint
 rule annotate_breakpoint:
@@ -206,8 +221,8 @@ rule annot_sv:
 
 rule visualize_exon:
     input:
-        bialsv_file = "analysis/bubble/{asb}_bialsv_stat.tsv",
-        multisv_file = "analysis/bubble/{asb}_multisv_stat.tsv",
+        bialsv_file = "analysis/bubble/{asb}_biallelic_sv.tsv",
+        multisv_file = "analysis/bubble/{asb}_multiallelic_sv.tsv",
         annot_file = "analysis/bubble/{asb}_breakpoint_annot.tsv"
     output: "analysis/bubble/{asb}_exon_viz.pdf"
     threads: 10
