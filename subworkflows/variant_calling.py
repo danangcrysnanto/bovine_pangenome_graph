bdbb8cb0f17dba6417bc9a0980218fd20bad5e7f
danangcrysnanto <danang.crysnanto@usys.ethz.ch>
2020-12-06 15:29:14 +0100

    Add samtools variant calling

--- subworkflows/variant_calling.py
+++ subworkflows/variant_calling.py
@@ -6,13 +6,14 @@ workdir: config["workdir"]
 
 graph_list = config["graph"]
 BAMDIR = config["bamdir"]
-# REF = f"wgs/reference/{graph}pan.fa"
 knowvar = config["knowvar"]
 
 SAMPLES, = glob_wildcards(f"wgs/bam/{{sample}}_{graph_list}pan.bam")
 
 rule all:
-    input: expand("wgs/varcall/vcf/nonref_{graph}.vcf.gz", graph=graph_list)
+    input:
+        expand("wgs/varcall/nonref_{graph}_gatk.vcf.gz", graph=graph_list),
+        expand("wgs/varcall/nonref_{graph}_samtools_filtered.vcf.gz", graph=graph_list)
 
 # make bed files out of non-ref sequnces
 localrules: create_nonref_bed
@@ -94,7 +95,6 @@ rule recalibrator_creator:
 
         """
 
-
 rule base_recalibrator:
     input:
         recal = rules.recalibrator_creator.output,
@@ -148,58 +148,41 @@ rule Haplotype_caller:
 
         """
 
-# rule GenomicsDB_import:
-    # input:
-        # gvcf = expand("wgs/gvcf/{sample}_{{graph}}.g.vcf.gz", sample=SAMPLES),
-        # ref = "wgs/reference/{graph}pan.fa",
-        # nonref_file = "wgs/varcall/{graph}_nonref.list"
-    # output:
-        # samp_map = "wgs/varcall/db_imp/nonref_{graph}.map",
-        # db_dir = directory(temp("wgs/varcall/db_imp/db_nonref_{graph}"))
-    # threads: 10
-    # resources:
-        # mem_mb = 5000,
-        # walltime = "04:00"
-    # envmodules:
-        # "gcc/4.8.5",
-        # "jdk/8u172-b11"
-    # shell:
-        # """
+checkpoint split_bed:
+    input: "wgs/varcall/{graph}_nonref.list"
+    output: directory("wgs/split_bed_{graph}")
+    threads: 1
+    resources:
+        mem_mb = 500,
+        walltime = "00:10",
+    params:
+        split_size = 100
+    shell:
+        """
 
-        # echo {input.gvcf} |
-        # tr " " "\\n"|
-        # awk '{{ split($1,arr,"/");
-        # split(arr[3],samp, "_");
-        # print samp[1]"\\t"$1 }}' > {output.samp_map}
+        mkdir -p {output}
 
-        # gatk GenomicsDBImport \
-        # --sample-name-map {output.samp_map} \
-        # --genomicsdb-workspace-path {output.db_dir} \
-        # --batch-size 45 \
-        # --reader-threads 10 \
-        # -L {input.nonref_file} \
-        # -R {input.ref}
+        split -d -a 3 -l {params.split_size} {input} {output}/part
 
-        # """
+        """
 
-rule Combine_gvcfs:
+
+rule call_vcf:
     input:
         gvcf = expand("wgs/gvcf/{sample}_{{graph}}.g.vcf.gz", sample=SAMPLES),
         ref = "wgs/reference/{graph}pan.fa",
         ref_dict = "wgs/reference/{graph}pan.dict",
-        nonref_file = "wgs/varcall/{graph}_nonref.list"
+        nonref_file = "wgs/split_bed_{graph}/part{part}"
     output:
-        combgv = "wgs/comb/{graph}.g.vcf.gz"
+        protected("wgs/varcall/vcf/nonref_{graph}_{part}.vcf.gz")
     threads: 10
     resources:
         mem_mb = 5000,
         walltime = "04:00",
-        disk_scracth = 100
+        disk_scratch = 50
     envmodules:
         "gcc/4.8.5",
         "jdk/8u172-b11"
-    params:
-        sample_inf = " ".join([f"--variant {samp}_{{graph}}.g.vcf.gz" for samp in SAMPLES])
     shell:
         """
 
@@ -211,37 +194,118 @@ rule Combine_gvcfs:
 
         cd $TMPDIR
 
-        gatk CombineGVCFs \
-         -R {wildcards.graph}pan.fa \
-         {params.sample_inf} \
-         -L {wildcards.graph}_nonref.list \
-         -O comb_out.g.vcf.gz
+        mv part{wildcards.part} part{wildcards.part}.list
 
-         cp comb_out.g.vcf.gz $LS_SUBCWD/{output}
+        echo {input.gvcf} |
+        tr " " "\\n"|
+        awk '{{ split($1,arr,"/");
+        split(arr[3],samp, "_");
+        print samp[1]"\\t"arr[3] }}' > out.samp
+
+        gatk GenomicsDBImport \
+            --sample-name-map out.samp \
+            --genomicsdb-workspace-path db_comb \
+            --reader-threads 8 \
+            -L part{wildcards.part}.list \
+            -R {wildcards.graph}pan.fa
+
+
+        gatk GenotypeGVCFs \
+            -R {wildcards.graph}pan.fa \
+            -L part{wildcards.part}.list \
+            -O out.vcf \
+            -V gendb://db_comb
+
+        cp out.vcf $LS_SUBCWD/{output}
 
         """
 
-rule Joint_Genotyping:
-    input:
-        combgv = "wgs/comb/{graph}.g.vcf.gz",
-        ref = "wgs/reference/{graph}pan.fa",
-        nonref_file = "wgs/varcall/{graph}_nonref.list"
-    output:
-        protected("wgs/varcall/vcf/nonref_{graph}.vcf.gz")
+
+def get_part(wildcards):
+    """
+
+    Return splitted part region of bed
+
+    """
+
+    checkpoint_output = checkpoints.split_bed.get(**wildcards).output[0]
+    all_parts, = glob_wildcards(checkpoint_output + "/part{part}")
+    print(all_parts)
+
+    return (f"wgs/varcall/vcf/nonref_{wildcards.graph}_{part}.vcf.gz" for part in all_parts)
+
+
+rule combine_vcf:
+    input: get_part
+    output: "wgs/varcall/nonref_{graph}_gatk.vcf.gz"
     threads: 10
     resources:
-        mem_mb = 5000,
-        walltime = "12:00"
-    envmodules:
-        "gcc/4.8.5",
-        "jdk/8u172-b11"
+        mem_mb = 1000,
+        walltime = "01:00"
     shell:
         """
 
-        gatk GenotypeGVCFs \
-            -R {input.ref} \
-            -L {input.nonref_file} \
-            -O {output} \
-            -V {input}
+        echo {input} |
+        tr " " "\\n" > file_list.temp
+
+        bcftools concat --threads {threads} --file-list file_list.temp -O z -o {output}
+
+
+        """
+
+localrules: create_samtools_bed
+rule create_samtools_bed:
+    input: "analysis/bubble/{graph}_nonrefsv.fa"
+    output: "wgs/varcall/{graph}_nonref.bed"
+    run:
+
+        def create_nonref_bed(infile):
+            for line in infile:
+                if line.startswith(">"):
+                    linecomp = line.strip()[1:]
+                    seqcon = next(infile)
+                    lencon = len(seqcon.strip())
+                    yield f"{linecomp}\t1\t{lencon}"
+
+        with open(input[0]) as infile, open(output[0], "a") as outfile:
+            for comp in create_nonref_bed(infile):
+                outfile.write(f"{comp}\n")
+
+rule call_samtools:
+    input:
+        bam = expand("wgs/bam_recal/{sample}_{{graph}}_recalibrated.bam", sample=SAMPLES),
+        ref = "wgs/reference/{graph}pan.fa",
+        bed = "wgs/varcall/{graph}_nonref.bed"
+    output: "wgs/varcall/nonref_{graph}_samtools.vcf.gz"
+    threads: 10
+    resources:
+        mem_mb = 1000,
+        walltime = "04:00"
+    shell:
+        """
+
+        echo {input} |
+        tr " " "\\n" > bamlist.temp
+
+        samtools mpileup -uf {input.ref} --positions {input.bed} -E \
+          -t DP -t SP -t ADF -t ADR -t AD --bam-list bamlist.temp|
+          bcftools call -mv -o {output} -O z
+
+
+        """
+
+rule filter_samtools:
+    input: "wgs/varcall/nonref_{graph}_samtools.vcf.gz"
+    output: "wgs/varcall/nonref_{graph}_samtools_filtered.vcf.gz"
+    threads: 10
+    resources:
+        mem_mb = 1000,
+        walltime = "00:10"
+    shell:
+        """
+
+        bcftools filter -e "QUAL < 20 || INFO/MQ < 30 || INFO/DP < 10 || INFO/AN < 10" \
+                -o {output} -O z {input}
+
 
         """
